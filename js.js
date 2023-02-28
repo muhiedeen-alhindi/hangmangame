763f773b4db4c08c8c3dcd8357bbac426206790d
muhiedeen <muhiedeen.alhindi@gmail.com>
2023-02-28 03:14:31 +0200

    some modification

--- js.js
+++ js.js
@@ -19,14 +19,7 @@ const p=document.getElementById("p").innerText
 let text1=document.getElementById("text1")
 let text2=document.getElementById("text2")
 let text3=document.getElementById("text3")
- let div1=document.getElementById("rope")
- let div2 =document.getElementById("body" )  
- let div3=document.getElementById("left-arm")  
- let div4=document.getElementById("right-arm")  
- let div5=document.getElementById("left-leg")  
- let div6=document.getElementById("right-leg") 
- let div7=document.getElementById("head")  
-            
+  
              
          
 
@@ -51,7 +44,9 @@ const text = {
     } else {
         if (x <= 7) {
             document.getElementById("div" + x).style.display = "block";
+
             x++;
     }
   }
 }
+ 
