983cbb8c58770252ea94bb353bb8808bddd45b72
muhiedeen <muhiedeen.alhindi@gmail.com>
2023-02-28 03:10:59 +0200

    some modification in fucntion

--- js.js
+++ js.js
@@ -19,26 +19,39 @@ const p=document.getElementById("p").innerText
 let text1=document.getElementById("text1")
 let text2=document.getElementById("text2")
 let text3=document.getElementById("text3")
+ let div1=document.getElementById("rope")
+ let div2 =document.getElementById("body" )  
+ let div3=document.getElementById("left-arm")  
+ let div4=document.getElementById("right-arm")  
+ let div5=document.getElementById("left-leg")  
+ let div6=document.getElementById("right-leg") 
+ let div7=document.getElementById("head")  
+            
+             
+         
 
 const text = {
     first: 'c',
     second: 'a',
-    third:   'r',
+    third:   'p',
   };
  console.log(text.first)
 
-
+ let x=1
  function checkLetter(button) {
     const letter = button.textContent.toLowerCase(); 
      
     if (letter === text.first) {
         text1.textContent = letter;
-      alert("woho")
+       
     } else if (letter === text.second) {
       text2.textContent = letter;
     } else if (letter === text.third) {
         text3.textContent = letter;
     } else {
-       
+        if (x <= 7) {
+            document.getElementById("div" + x).style.display = "block";
+            x++;
     }
   }
+}
