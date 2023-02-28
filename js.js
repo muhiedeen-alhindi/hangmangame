1659a52d5d704c6f94cc96c064472fab59d7c0f1
muhiedeen <muhiedeen.alhindi@gmail.com>
2023-02-28 02:10:50 +0200

    js add some let

--- js.js
+++ js.js
@@ -1,1 +1,20 @@
+let a=document.getElementById("a").innerText
+let b=document.getElementById("b").innerText
+let c=document.getElementById("c").innerText
+let d=document.getElementById("d").innerText
+let e=document.getElementById("e").innerText
+let f=document.getElementById("f").innerText
+let g=document.getElementById("g").innerText
+let h=document.getElementById("h").innerText
+let i=document.getElementById("i").innerText
+let j=document.getElementById("j").innerText
+let k=document.getElementById("k").innerText
+let l=document.getElementById("l").innerText
+let m=document.getElementById("m").innerText
+let n=document.getElementById("n").innerText
+let o=document.getElementById("o").innerText
+let p=document.getElementById("p").innerText
+
+let text 
+
  
